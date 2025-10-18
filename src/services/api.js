b252fb499b610ad2271b6a0ba47a911923d058fa
srithareddy12 <sirilsritha.reddy@gmail.com>
2025-10-18 22:39:01 +0530

    fir

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
     import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://backend-1-ovie.onrender.com/api",
+  //baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL:"https://backend-1-ovie.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
